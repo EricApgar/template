66600f8620f6e64effe05b78de68b23082b94839
Eric Apgar <ericapgar1@gmail.com>
2026-02-07 20:11:41 -0600

    Updating pyproject and init.

--- README.md
+++ README.md
@@ -2,7 +2,30 @@
 Description of the repo.
 
 # Setup
-...
+Working with this repo as a library or a standalone repo.
+
+## Library
+Use as a library and install as a dependency into an existing project.
+
+```
+uv add git+<full path to GitHub Repo>@<release tag>
+```
+
+```
+uv add "library[option_1,option2] @ git+<full path to GitHub Repo>@<release tag>"
+```
+
+```
+pip install git+<full path to GitHub Repo>@<release tag>
+```
+
+```uv add library``` installs only [project.dependencies]  
+```uv add "library[phi4]"``` installs [project.dependencies] plus the phi4 optional deps  
+```uv add "library[phi4,gpt_oss]"``` installs baseline plus union of both extras  
+```uv add "library[all]"``` baseline plus whatever you put in all
+
+## Repo
+Run locally for development of this repo.
 
 # Usage
 ...
---
+++ src/template/__init__.py
@@ -0,0 +1,42 @@
+'''
+Functionality for example namespace:
+
+import template
+
+ns = template.item_1()
+'''
+from __future__ import annotations
+from typing import TYPE_CHECKING, Any
+
+
+# Public, supported “namespaces”.
+__all__ = ("item_1", "item_2")
+
+
+# Helps IDEs/type-checkers know these exist and what they are,
+# without importing them at runtime.
+if TYPE_CHECKING:
+    from .item_folder.item_1_file import item_1
+    from .item_folder.item_2_file import item_2
+
+
+def __getattr__(name: str):
+    if name == 'item_1':
+        try:
+            from .item_folder.item_1_file import item_1
+            globals()[name] = item_1  # Recommended for smoother checking.
+        except ImportError:
+            raise ImportError('Missing packages for "namespace"! Install with "template[namespace]" or "template[all]".')
+        return item_1
+
+    if name == 'item_2':
+        from .item_folder.item_2_file import item_2
+        globals()[name] = item_2  # Recommended for smoother checking.
+        return item_2
+
+    raise AttributeError(f'Module "<lib name>" has no attribute {name!r}!')  # TODO: Edit your library name.
+
+
+def __dir__() -> list[str]:
+    # Makes dir(llm) include your lazy “namespaces”
+    return sorted(set(globals().keys()) | set(__all__))
